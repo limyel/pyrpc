107557732fbd8d96ba363929771d32793641de5a
limyel <limyel@outlook.com>
2022-03-13 16:12:05 +0800

    multiprocess

---
+++ server/multiprocess.py
@@ -0,0 +1,67 @@
+import json
+import os
+import socket
+import struct
+import threading
+
+
+def handle_conn(conn: socket.socket, addr, handlers):
+    print(addr, 'comes')
+    while True:
+        length_prefix = conn.recv(4)
+        if not length_prefix:
+            print(addr, 'bye')
+            conn.close()
+            break
+        length, = struct.unpack('I', length_prefix)
+        body = conn.recv(length)
+        request = json.loads(body)
+        in_ = request.get('in')
+        params = request.get('params')
+        print(in_, params)
+        handler = handlers.get(in_)
+        handler(conn, params)
+
+
+def loop(sock: socket.socket, handlers):
+    while True:
+        conn, addr = sock.accept()
+        # 创建子进程处理新连接
+        pid = os.fork()
+        if pid < 0:
+            # 异常
+            return
+        if pid > 0:
+            # 父进程
+            # 关闭父进程的连接套接字
+            conn.close()
+            continue
+        if pid == 0:
+            # 子进程
+            # 关闭子进程的服务器套接字
+            sock.close()
+            handle_conn(conn, addr, handlers)
+            # 处理完要跳出循环，不然子进程也会 accept() 请求
+            break
+
+
+def ping(conn, params):
+    send_result(conn, 'pong', params)
+
+
+def send_result(conn: socket.socket, out, result):
+    response = json.dumps({'out': out, 'result': result})
+    length_prefix = struct.pack('I', len(response))
+    conn.sendall(length_prefix)
+    conn.sendall(response.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('localhost', 8080))
+    sock.listen(1)
+    handlers = {
+        'ping': ping
+    }
+    loop(sock, handlers)
